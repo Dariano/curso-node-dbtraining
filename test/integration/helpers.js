75a098475a4c698bc2765c1b5b92ce6a32ec16b6
Dariano <darianos@dbserver.com.br>
2017-11-08 11:44:03 -0200

    Adicionado autenticacao

--- test/integration/helpers.js
+++ test/integration/helpers.js
@@ -2,8 +2,10 @@ const supertest = require('supertest')
 const chai = require('chai')
 const app = require('../../src/app')
 const models = require('../../src/models')
+const config = require('../../src/config/config')[process.env.NODE_ENV]
 
 global.app = app
 global.request = supertest(app)
 global.expect = chai.expect
+global.config = config
 global.models = models
